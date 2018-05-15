8b3fa1925ef123e850f3c9e19b3ae01f214b3eda
Travis Whitaker <pi.boy.travis@gmail.com>
2018-05-15 00:58:57 -0700

    First kinda working instance message.

--- src/Cocoa/Foundation/NSString/Msg.hs
+++ src/Cocoa/Foundation/NSString/Msg.hs
@@ -3,6 +3,8 @@
 
 module Cocoa.Foundation.NSString.Msg where
 
+import Foreign.Ptr
+
 import GHC.TypeLits
 
 import Language.ObjC.MsgSend
@@ -13,3 +15,13 @@ import Cocoa.Foundation.NSString.Type
 -- | Returns an empty 'NSString'.
 string :: IO NSString
 string = $(mkSendClassMsg [t| NSString |] "string" [t| IO NSString |])
+
+-- | Initialize an NSString with the given byte buffer and encoding.
+initWithBytes :: NSString -> Ptr () -> Int -> Int -> IO NSString
+initWithBytes = $(mkSendInstMsg "initWithBytes"
+                                [t| NSString
+                                 -> Ptr ()
+                                 -> Int
+                                 -> Int
+                                 -> IO NSString |]
+                 )
