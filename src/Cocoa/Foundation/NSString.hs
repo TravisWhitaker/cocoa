38713883223a77f7645a70026c8ed2893f9cc815
Travis Whitaker <pi.boy.travis@gmail.com>
2018-06-02 13:41:13 -0700

    Got 'em.

--- src/Cocoa/Foundation/NSString.hs
+++ src/Cocoa/Foundation/NSString.hs
@@ -1,12 +1,21 @@
 module Cocoa.Foundation.NSString (
     NSString()
   , copyFromByteString
+  , copyFromText
+  , copyToText
   ) where
 
 import qualified Data.ByteString          as BS
 import qualified Data.ByteString.Internal as BS
 
+import qualified Data.Text          as T
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.Foreign  as T
+
+import Debug.Trace
+
 import Foreign.ForeignPtr
+import Foreign.Ptr
 
 import Language.ObjC.NSObject
 
@@ -14,11 +23,6 @@ import Cocoa.Foundation.NSString.Type
 import Cocoa.Foundation.NSString.Msg
 
 import Cocoa.Foundation.NSStringEncoding
-
-copyFromByteString :: NSStringEncoding -> BS.ByteString -> IO NSString
-copyFromByteString nsse bs = do
-    nssi <- alloc
-    initWithByteString nsse nssi bs
 
 initWithByteString :: NSStringEncoding
                    -> NSString
@@ -29,3 +33,27 @@ initWithByteString nsse nss bs =
         nsseCode         = nsStringEncodingToCULong nsse
     in withForeignPtr (bsfp `plusForeignPtr` off) $ \p ->
         initWithBytes nss p (fromIntegral len) nsseCode
+
+copyFromByteString :: NSStringEncoding -> BS.ByteString -> IO NSString
+copyFromByteString nsse bs = do
+    nssi <- alloc
+    initWithByteString nsse nssi bs
+
+copyFromText :: T.Text -> IO NSString
+copyFromText t =
+    T.useAsPtr t (\p l -> stringWithCharacters (castPtr p) (fromIntegral l))
+
+copyToText :: NSString -> IO T.Text
+copyToText nss = do
+    utf16Len <- utf16Length nss
+    let byteLen         = utf16Len * 2
+        byteLenWithNull = byteLen + 2
+        enc             = nsStringEncodingToCULong NSUTF16StringEncoding
+    fp <- mallocForeignPtrBytes (fromIntegral byteLenWithNull)
+    r  <- withForeignPtr
+            (castForeignPtr fp)
+            (\p -> getCString nss p (fromIntegral byteLenWithNull) enc)
+    let bs = BS.fromForeignPtr fp 0 (fromIntegral byteLen)
+    pure $ if (r == 1)
+           then Just (T.decodeUtf16LE bs)
+           else Nothing
--- src/Cocoa/Foundation/NSString/Msg.hs
+++ src/Cocoa/Foundation/NSString/Msg.hs
@@ -27,3 +27,49 @@ initWithBytes = $(mkSendInstMsg "initWithBytes:length:encoding:"
                                  -> CULong
                                  -> IO NSString |]
                  )
+
+-- | Initialize an NSString with the given UTF-16 byte buffer.
+initWithCharacters :: NSString -> Ptr () -> CSize -> IO NSString
+initWithCharacters = $(mkSendInstMsg "initWithCharacters:length:"
+                                     [t| NSString
+                                      -> Ptr ()
+                                      -> CSize
+                                      -> IO NSString |]
+                      )
+
+-- | Initialize an NSString by copying another NSString.
+initWithString :: NSString -> NSString -> IO NSString
+initWithString = $(mkSendInstMsg "initWithString:"
+                                 [t| NSString
+                                  -> NSString
+                                  -> IO NSString |]
+                  )
+
+-- | Create an NSString by copying another NSString.
+stringWithString :: NSString -> IO NSString
+stringWithString = $(mkSendClassMsg [t| NSString |]
+                                    "stringWithString:"
+                                    [t| NSString -> IO NSString |]
+                    )
+
+-- | Create an NSString with the given UTF-16 byte buffer.
+stringWithCharacters :: Ptr () -> CSize -> IO NSString
+stringWithCharacters = $(mkSendClassMsg [t| NSString |]
+                                        "stringWithCharacters:length:"
+                                        [t| Ptr () -> CSize -> IO NSString |]
+                        )
+
+getCString :: NSString -> Ptr () -> CSize -> CULong -> IO CUChar
+getCString = $(mkSendInstMsg "getCString:maxLength:encoding:"
+                             [t| NSString
+                              -> Ptr ()
+                              -> CSize
+                              -> CULong
+                              -> IO CUChar |]
+              )
+
+utf16Length :: NSString -> IO CULong
+utf16Length = $(mkSendInstMsg "length"
+                              [t| NSString
+                               -> IO CULong |]
+               )
